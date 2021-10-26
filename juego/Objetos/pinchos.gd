6a9c8dac8bd9a427a4c4bdedbd25ab4410a05694
Fer <foclemens@gmail.com>
2021-10-26 15:28:45 -0300

    portal hecho

--- juego/Objetos/pinchos.gd
+++ juego/Objetos/pinchos.gd
@@ -1,17 +1,20 @@
 extends Area2D
 
-export var es_trampa := false
+export var es_trampa = false
 onready var detector_personaje = $Detector
+var color_trampa = Color.red
+onready var animacion = $AnimationPlayer
 
 func _ready():
 	if es_trampa:
+		$Sprite.modulate = color_trampa
 		detector_personaje.enabled = true
 		rotation_degrees = 180
 
 func _process(delta):
 	if detector_personaje.is_colliding():
-		pass
-	
+		detector_personaje.set_deferred("enable", false)
+		animacion.play("caer")
 
 func _on_body_entered(body):
 	body.respawn()
